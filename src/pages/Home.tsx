ea211e7301520f72aae39301bc17ea10fe83108e
rgquiet <robinguedel@gmail.com>
2021-01-25 23:49:26 +0100

    Test Filesystem API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,58 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonModal, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { FilesystemEncoding, FilesystemDirectory, Plugins } from '@capacitor/core';
+import React, { useState } from 'react';
 import './Home.css';
 
+const { Filesystem } = Plugins;
+
 const Home: React.FC = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [files, setFiles] = useState(['']);
+
+    const writeHandler = () => {
+        const fileName = 'Download/' + new Date().getTime() + '.txt';
+        Filesystem.writeFile({
+            path: fileName,
+            data: 'Hello World',
+            directory: FilesystemDirectory.ExternalStorage,
+            encoding: FilesystemEncoding.UTF8
+        }).then(uri => console.log(uri));
+    }
+
+    const readHandler = () => {
+        Filesystem.readdir({
+            path: '',
+            directory: FilesystemDirectory.ExternalStorage
+        }).then(r => {
+            setFiles(r.files);
+            setShowModal(true);
+        });
+    }
+
+    const openFile = () => {
+        /* wip:
+        https://capacitorjs.com/docs/apis/filesystem
+        https://stackoverflow.com/questions/62444931/capacitor-write-files-to-download-folder
+        */
+    }
+
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>Home</IonTitle>
+                    <IonTitle>Test: Filesystem</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent fullscreen>
-                <p>wip...</p>
+            <IonContent class='ion-padding' fullscreen>
+                <IonButton onClick={writeHandler}>Write</IonButton>
+                <IonButton onClick={readHandler}>Read</IonButton>
+                <IonModal isOpen={showModal}>
+                    <h2>Choose a file</h2>
+                    {files.map((file, i) => (
+                        <p>{i}: {file}</p>
+                    ))}
+                    <IonButton onClick={() => setShowModal(false)}>Exit</IonButton>
+                </IonModal>
             </IonContent>
         </IonPage>
     );
